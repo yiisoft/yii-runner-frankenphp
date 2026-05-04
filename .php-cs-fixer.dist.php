ad92aff8811d5eb4ddeb86112e4e510bb9c08275
Tigrov <rrr-r@ya.ru>
2026-05-04 17:00:45 +0700

    Fix rector-cs.yml

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,27 +2,20 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+use Yiisoft\CodeStyle\ConfigBuilder;
 
-return (new Config())
-    ->setRiskyAllowed(false)
+$finder = (new Finder())->in([
+    __DIR__ . '/src',
+    __DIR__ . '/tests',
+]);
+
+return ConfigBuilder::build()
+    ->setRiskyAllowed(true)
+    ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRules([
-        '@auto' => true
+        '@Yiisoft/Core' => true,
+        '@Yiisoft/Core:risky' => true,
     ])
-    // 💡 by default, Fixer looks for `*.php` files excluding `./vendor/` - here, you can groom this config
-    ->setFinder(
-        (new Finder())
-            // 💡 root folder to check
-            ->in(__DIR__)
-            // 💡 additional files, eg bin entry file
-            // ->append([__DIR__.'/bin-entry-file'])
-            // 💡 folders to exclude, if any
-            // ->exclude([/* ... */])
-            // 💡 path patterns to exclude, if any
-            // ->notPath([/* ... */])
-            // 💡 extra configs
-            // ->ignoreDotFiles(false) // true by default in v3, false in v4 or future mode
-            // ->ignoreVCS(true) // true by default
-    )
-;
+    ->setFinder($finder);
